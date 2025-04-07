e85cafc344cc23353c96cfaa0b16dcd608b89fea
stemyy <Himanbro112233@gmail.com>
2025-04-07 19:47:35 -0400

    camera testing

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hellow world")
+import cv2
+
+stream = cv2.VideoCapture(0)
+
+if not stream.isOpened():
+    print("No Stream")
+    exit()
+
+while(True):
+    ret, frame = stream.read()
+    frame = cv2.flip(frame,1)
+    if not ret:
+        print("No More Stream")
+        break
+
+    cv2.imshow("Pong", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
