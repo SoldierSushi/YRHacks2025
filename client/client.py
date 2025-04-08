cc5ddd8f3786f160f250e9c933b7bf63649a1b3f
Himanbro112233@gmail.com <Himanbro112233@gmail.com>
2025-04-08 12:37:40 -0400

    hopefully fix

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ import json
 from protocols import Protocols
 
 class Client:
-    def __init__(self, host="192.168.1.10", port=8001):
+    def __init__(self, host="127.0.0.1", port=8001):
         self.nickname = None
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((host, port))
--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from room import Room
 import time
 
 class Server:
-    def __init__(self, host="192.168.1.10", port=8001):
+    def __init__(self, host="127.0.0.1", port=8001):
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
